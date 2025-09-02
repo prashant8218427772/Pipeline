21e552ef98a2cec5cd5521a23bba11949e28673e
prashant8218427772 <Prashant.Verma7772@gmail.com>
2025-09-02 22:45:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,12 @@ terraform {
 
 provider "azurerm" {
     features {}
-    subscription_id = "325754cd-a12c-48d9-921b-eb17c173bd01"
+    subscription_id = "81cc1915-8d88-419e-8fa3-0178811761bd"
     }
   
 resource "azurerm_resource_group" "prashant-rg" {
   name = "Prashant-rg1"
   location = "Central India"
   
+
 }
